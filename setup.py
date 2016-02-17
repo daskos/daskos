1ac85a457667be767aa539dc86c59aaf1d6f1568
Szucs Krisztian <szucs.krisztian@gmail.com>
2016-02-17 14:49:24 +0100

    setup.py

---
+++ setup.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+from os.path import exists
+from setuptools import setup
+import debas
+
+setup(name='dask.mesos',
+      version='0.1',
+      description='A Mesos executor backend for Dask',
+      long_description=(open('README.rst').read() if exists('README.rst')
+                        else ''),
+      url='http://github.com/lensacom/dask.mesos',
+      maintainer='Zoltan Nagy',
+      maintainer_email='zoltan.nagy@lensa.com',
+      license='BSD',
+      keywords='mesos dask multiprocessing',
+
+      packages=['dask_mesos'],
+      install_requires=['dask', 'satyr'],
+      setup_requires=['pytest-runner'],
+      tests_require=['pytest'],
+      dependency_links=[
+          'git+ssh://git@github.com/lensacom/satyr.git'],
+      zip_safe=False)
