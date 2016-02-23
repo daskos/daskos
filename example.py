23f1b1ad798e755136fada15ee1becf686869bab
Zoltan Nagy <zoltan.nagy@replise.com>
2016-02-23 01:08:09 +0100

    example updated w/ some textz

--- example.py
+++ example.py
@@ -6,22 +6,27 @@ from dask_mesos.imperative import mesos
 from dask_mesos.mesos import get
 
 
-@mesos(mem=512, cpus=1, image='bdas-master-3:5000/satyr')
+"""Run stuff on mesos w/o docker but w/ resources set."""
+@mesos(mem=512, cpus=1)
 def inc(x):
     return x + 1
 
 
-@mesos(mem=512, cpus=1, image='bdas-master-3:5000/satyr')
+"""Run stuff on mesos w/o docker and w/ default resource setup."""
+@do
 def add(x, y):
     return x + y
 
 
+"""Run stuff on mesos w/ docker and specified resources."""
 @mesos(mem=512, cpus=1, image='bdas-master-3:5000/satyr')
 def mul(x, y):
     return x * y
 
 
+"""This context ensures that @do will also run on mesos."""
 with set_options(get=get):
     one = inc(0)
     alot = add(one, 789)
-    print(mul(alot, 987).compute())
+    gigalot = mul(alot, inc(alot))
+    print(mul(alot, gigalot).compute())
