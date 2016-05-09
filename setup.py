697abbbbaef23132f02f928196b61fe99c7be7af
Krisztián Szűcs <szucs.krisztian@gmail.com>
2016-05-09 15:54:55 +0200

    Update setup.py

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup
 
 setup(name='dask.mesos',
       version='0.1.2',
-      description='A Mesos executor backend for Dask',
+      description='Apache Mesos backend for Dask scheduling library',
       long_description=(open('README.md').read() if exists('README.md')
                         else ''),
       url='http://github.com/lensacom/dask.mesos',
