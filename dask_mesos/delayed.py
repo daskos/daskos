077938925667f1f39e69903ca76b8bed0bc3fbb3
Szucs Krisztian <szucs.krisztian@gmail.com>
2016-05-25 10:21:07 +0200

    Updated drone build

--- dask_mesos/delayed.py
+++ dask_mesos/delayed.py
@@ -6,7 +6,6 @@ from satyr.proxies.messages import Cpus, Disk, Mem
 from .satyr import SatyrPack
 
 
-
 @curry
 def mesos(obj, name=None, pure=True, cpus=1, mem=64, disk=0, **kwargs):
     kwargs['resources'] = [Cpus(cpus), Mem(mem), Disk(disk)]
