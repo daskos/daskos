da5be29cf4b393005cd3887a2d34ee2febc4626d
Szucs Krisztian <szucs.krisztian@gmail.com>
2016-06-14 11:29:23 +0200

    Unset default logging level INFO

--- dask_mesos/__init__.py
+++ dask_mesos/__init__.py
@@ -1,12 +1,13 @@
 from __future__ import absolute_import, division, print_function
 
 import logging
+import pkg_resources as _pkg_resources
 
 from .delayed import mesos
 from .executor import MesosExecutor, get
 
+logging.basicConfig(format='%(relativeCreated)6d %(threadName)s %(message)s')
 
-logging.basicConfig(level=logging.INFO,
-                    format='%(relativeCreated)6d %(threadName)s %(message)s')
+__version__ = _pkg_resources.get_distribution('dask.mesos').version
 
 __all__ = ('MesosExecutor', 'get', 'mesos')
